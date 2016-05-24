5324a390273a6000cad9691053b7f7a025932809
Eze BIT <efigueroa@bit.com.ar>
2016-05-24 11:41:10 -0300

    Back control

--- www/js/functions.js
+++ www/js/functions.js
@@ -197,3 +197,41 @@ function loadNav()
 {
 	$("#nav").load("menu.html"); 
 }
+
+function controlBack(source)
+{
+	window.onload = function () {
+		if (typeof history.pushState === "function") {
+			history.pushState("jibberish", null, null);
+			window.onpopstate = function () {
+				if(source == 'inicio')
+				{
+					history.pushState('newjibberish', null, null);
+					navigator.app.exitApp();
+				}
+				else
+				{
+					window.location.href = "inicio.html";	
+				}
+				// Handle the back (or forward) buttons here
+				// Will NOT handle refresh, use onbeforeunload for this.
+			};
+		}
+		else {
+			var ignoreHashChange = true;
+			window.onhashchange = function () {
+				if (!ignoreHashChange) {
+					ignoreHashChange = true;
+					window.location.hash = Math.random();
+					// Detect and redirect change here
+					// Works in older FF and IE9
+					// * it does mess with your hash symbol (anchor?) pound sign
+					// delimiter on the end of the URL
+				}
+				else {
+					ignoreHashChange = false;   
+				}
+			};
+		}
+	}
+}
